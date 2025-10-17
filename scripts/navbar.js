977d5d57a7307e6db9470664a1095fc6a22b276b
Tomás Volpini <tomasvolpini@gmail.com>
2025-10-16 23:56:17 -0300

    mobile menu drawer added, both style and script

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -18,3 +18,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const button = document.getElementById("collapsed-menu");
+const symbol = document.querySelector("#collapsed-menu span");
+const menuList = document.getElementById("nav-list");
+const li = document.querySelectorAll("#nav-list li");
+const navbar = document.querySelector("nav");
+const overlay = document.createElement("div");
+overlay.id = "nav-overlay";
+navbar.appendChild(overlay);
+
+li.forEach((l) => {
+  l.addEventListener("click", () => {
+    menuList.classList.remove("active");
+    overlay.classList.remove("active");
+    symbol.innerHTML = "menu";
+  });
+});
+
+button.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+  overlay.classList.toggle("active");
+  if (menuList.classList.contains("active")) {
+    symbol.innerHTML = "close";
+  } else {
+    symbol.innerHTML = "menu";
+  }
+});
+
+overlay.addEventListener("click", () => {
+  menuList.classList.remove("active");
+  overlay.classList.remove("active");
+  symbol.innerHTML = "menu";
+});
